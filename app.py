f7519eae39407794641c8e72caa85c4532d1fd16
vushkv13 <v.ushkv13@outlook.com>
2024-07-21 23:51:28 +0400

    Update app.py

--- app.py
+++ app.py
@@ -7,10 +7,6 @@ from douyin_tiktok_scraper.scraper import Scraper
 from dotenv import load_dotenv
 import logging
 
-# Настройка логирования
-logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger(__name__)
-
 logging.getLogger().setLevel(logging.CRITICAL)
 load_dotenv()
 
@@ -19,66 +15,53 @@ token = os.getenv("TOKEN")
 BOT_USERNAME = '@TikTokv_vbot'
 
 async def start_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    logger.info("Received start command")
-    await update.message.reply_text('Привет! ОТправь мне ссылку на ТикТок видео')
+    await update.message.reply_text('Support me on : https://www.paypal.me/ardha27')
 
 async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    logger.info("Received help command")
-    await update.message.reply_text('Please type something so I can respond')
+    await update.message.reply_text('Please type something so i can respond')
 
 async def custom_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    logger.info("Received custom command")
-    await update.message.reply_text('This is a custom command')
+    await update.message.reply_text('This is custom command')
 
 async def hybrid_parsing(url: str) -> dict:
     try:
-        logger.info(f"Parsing URL: {url}")
+        # Hybrid parsing(Douyin/TikTok URL)
         result = await api.hybrid_parsing(url)
-        if result is None:
-            logger.error(f"No result returned for URL: {url}")
-            return None
 
-        logger.info(f"Parsing result: {result}")
+        video = result["video_data"]["nwm_video_url_HQ"]
+        video_hq = result["video_data"]["nwm_video_url_HQ"]
+        music = result["music"]["play_url"]["uri"]
+        caption = result["desc"]
 
-        video = result.get("video_data", {}).get("nwm_video_url_HQ")
-        video_hq = result.get("video_data", {}).get("nwm_video_url_HQ")
-        music = result.get("music", {}).get("play_url", {}).get("uri")
-        caption = result.get("desc")
-
-        logger.info(f"Video URL: {video}")
-        logger.info(f"Video HQ URL: {video_hq}")
-        logger.info(f"Music URL: {music}")
-        logger.info(f"Caption: {caption}")
-
-        if not video or not video_hq or not music or not caption:
-            logger.error(f"Incomplete data for URL: {url}")
-            return None
-
+        print("Video URL:", video)
+        print("Video_HQ URL:", video_hq)
+        print("Play URL:", music)
+        print("Caption:", caption)
+        
         response_video = requests.get(video)
         response_video_hq = requests.get(video_hq)
 
         if response_video.status_code == 200:
             video_stream = BytesIO(response_video.content)
         else:
-            logger.error(f"Failed to download video. Status code: {response_video.status_code}")
-            return None
+            print(f"Failed to download MP4. Status code: {response_video.status_code}")
 
         if response_video_hq.status_code == 200:
             video_stream_hq = BytesIO(response_video_hq.content)
         else:
-            logger.error(f"Failed to download high quality video. Status code: {response_video_hq.status_code}")
-            return None
-
-        return video_stream, video_stream_hq, music, caption, video_hq
+            print(f"Failed to download MP4. Status code: {response_video_hq.status_code}")
+        
     except Exception as e:
-        logger.error(f'An error occurred: {str(e)}')
+        print(f'An error occurred: {str(e)}')
         return None
+
+    return video_stream, video_stream_hq, music, caption, video_hq
 
 async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
     message_type: str = update.message.chat.type
     text: str = update.message.text
 
-    logger.info(f'User ({update.message.chat.id}) in {message_type}: "{text}"')
+    print(f'User ({update.message.chat.id}) in {message_type}: "{text}"')
     if message_type == 'group':
         if BOT_USERNAME in text:
             new_text: str = text.replace(BOT_USERNAME, '').strip()
@@ -86,32 +69,36 @@ async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
             return
     elif message_type == 'private':
         if "tiktok.com" in text:
+
             result = await hybrid_parsing(text)
+
             if result:
-                video, video_hq, music, caption, link = result
-                text = f"Link:\n{link}\n\nSound:\n{music}\n\nCaption:\n{caption}"
-                text_link = f"Video is too large, sending link instead\n\nLink:\n{link}\n\nSound:\n{music}\n\nCaption:\n{caption}"
+                video = result[0]
+                video_hq = result[1]
+                music = result[2]
+                caption = result[3]
+                link =  result[4]
+                text = "Link:\n" + link + "\n\n" + "Sound:\n" + music + "\n\n" + "Caption:\n" + caption
+                text_link = "Video is too large, sending link instead" + "\n\n" + "Link:\n" + link + "\n\n" + "Sound:\n" + music + "\n\n" + "Caption:\n" + caption
 
                 try:
                     await update.message.reply_video(video=InputFile(video_hq), caption=text)
                 except Exception as e:
-                    logger.error(f"An error occurred while sending video: {str(e)}")
                     if "Request Entity Too Large (413)" in str(e):
-                        logger.info("Video is too large, sending link instead")
+                        print("Video is too large, sending link instead")
                         await update.message.reply_text(text_link)
-                    else:
-                        await update.message.reply_text("An error occurred while sending the video. Please try again later.")
+
             else:
-                await update.message.reply_text("An error occurred while parsing the TikTok URL. Please ensure the URL is correct and try again.")
+                await update.message.reply_text("Please send only TikTok URL")
         else:
             await update.message.reply_text("Please send a TikTok URL")
             return
 
 async def error(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    logger.error(f'Update {update} caused error {context.error}')
+    print(f'Update {update} caused error {context.error}')
 
 if __name__ == '__main__':
-    logger.info('Starting bot...')
+    print('Starting bot...')
     app = Application.builder().token(token).build()
 
     # Commands
@@ -127,5 +114,5 @@ if __name__ == '__main__':
     app.add_error_handler(error)
 
     # Polls the bot
-    logger.info('Polling...')
+    print('Polling...')
     app.run_polling(poll_interval=3)
